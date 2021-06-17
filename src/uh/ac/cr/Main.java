82939a23e72d61999d5ee3420f2c45048cd9184a
cartitos59 <cartitos59@gmail.com>
2021-06-16 22:02:59 -0600

    Ultimo while

--- src/uh/ac/cr/Main.java
+++ src/uh/ac/cr/Main.java
@@ -11,7 +11,7 @@ public class Main<decision> {
 
         //1- Todo. Ask user how many time he wants to print hello world on screen.
             //1.1- Todo. Print Hello world as many times as user defined. Use a while structure.
-        int i=1;
+        int i=1;//variable de control
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Ingrese cuantas veces desea imprimir Hello World.");
         int manytimes1 = scanner1.nextInt();
@@ -26,7 +26,7 @@ public class Main<decision> {
         Scanner scanner2 = new Scanner(System.in);
         System.out.println("Ingrese cuantas veces desea imprimir Hello World.");
         int manytimes2 = scanner2.nextInt();
-        for (i = 1; i<=manytimes2; i++)
+        for (i = 1; i<=manytimes2; i++) //toda la estructura del for
         {
             System.out.println("Hello World");
         }
@@ -34,7 +34,7 @@ public class Main<decision> {
         //2- Todo Print hello world and ask the user if he wants to print hello again using a Do while structure.
         System.out.println("-----------------------------");
         System.out.println("Hello World");
-        int user;
+        int user;//declarar desicio de usuario
         do {
             Scanner scanner3 = new Scanner(System.in);
             System.out.println("¿Desea imprimir Hello world? 1)SI; 2)NO");
@@ -47,8 +47,23 @@ public class Main<decision> {
         }
 
         //3- Todo ask the user if he wants to print hello using a while structure to keep printing until user wants to stop.
-
+        Scanner entrada = new Scanner (System.in);
+        int n1, n2, result, opcionNun; boolean op = true;
+        while (op) {
+            System.out.println("******");
+            System.out.println("menu");
+            System.out.println("1. continuar");
+            System.out.println("2. detener");
+            opcionNun = entrada.nextInt();
+            if (opcionNun == 2) {
+                System.out.println("finalizo");
+                break;
+            } else {
+                switch (opcionNun) {
+                    case 1:
+                        System.out.println("Hello mundo");
+                        break;
 
         //Do all the code needed for the previous items on the class created. Use main just to call the methods.
     }
-}
+}}}}
